bfa7a6368be2fce87f10fdad8583a1e084fffe95
mikolajsykula <m.sykula@sages.com.pl>
2023-01-27 19:12:32 +0100

    Add docker-compose

--- private-blog-backend/connection.js
+++ private-blog-backend/connection.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PW}@localhost:27017/mikolajs_blog`, (err) => {
+mongoose.connect(
+  `mongodb://${process.env.DB_USER}:${process.env.DB_PW}@localhost:27017/private_blog`,
+  (err) => {
     console.log(err);
     console.log('Connected to db');
-});
+  }
+);
